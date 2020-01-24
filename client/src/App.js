968966daaa7b6c8c442038d18d7b55b21a09dedb
almightyblob <dominik.antunovic@gmail.com>
2020-01-24 15:56:06 +0100

    backend: added models and routes for: customers, pets. Frontend: added form customers

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import './App.scss';
-import {Route} from "react-router-dom";
+import React from "react";
+import "./App.scss";
+import { Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "../src/components/auth/Signup";
 import Login from "../src/components/auth/Login";
+import AddCustomers from "./components/auth/AddCustomer";
 
 const App = () => {
   return (
     <div className="App">
-
       <Route exact path="/" component={Home} />
       <Route exact path="/signup" component={Signup} />
       <Route exact path="/login" component={Login} />
-
+      <Route exact path="/auth/addcustomer" component={AddCustomers} />
     </div>
   );
-}
+};
 
 export default App;
