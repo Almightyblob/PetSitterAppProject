19f40a265551cd7b938ec00e0765e3f59630eec2
Monika <mswidzinska87@gmail.com>
2020-01-30 21:09:08 +0100

    nicer 404 error page

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+
 class PageNotFound extends React.Component {
   render() {
     return (
       <div className="columns is-centered">
         <div className="column is-half has-text-centered">
-          <h2 className="is-size-2 has-text-danger has-text-weight-bold	">
-            The Page doesn't exist{" "}
+          <h2 className="is-size-2 has-text-danger has-text-weight-bold	has-margin-bottom-40">
+            The page doesn't exist
           </h2>
-          <p className="has-margin-bottom-40">
-            <Link to="/">Go to the Home Page </Link>
-          </p>
           <img src="/images/error.png" width="70%" alt="" />
         </div>
       </div>
