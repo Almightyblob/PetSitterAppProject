05246c2b4c7c21f3f6f405d31c9acc035daf0d37
almightyblob <dominik.antunovic@gmail.com>
2020-02-02 14:02:27 +0100

    refactored cutomerdetail= boxes are now individual components

--- client/src/pages/CustomerDetails.js
+++ client/src/pages/CustomerDetails.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 import "../App.scss";
 import Calendar from "../components/myCalendar";
-// import CustomerCard from "../components/layout/CustomerCard";
+import CustomerBox from "../components/CustomerBox";
+import PetBox from "../components/PetBox";
+import JobBox from "../components/JobBox";
 import { Link } from "react-router-dom";
 function CustomerDetails(props) {
   useEffect(() => {
@@ -22,42 +24,12 @@ function CustomerDetails(props) {
           <h3 className="is-size-3 has-text-weight-semibold has-text-primary has-text-centered-mobile has-margin-bottom-20">
             CUSTOMER
           </h3>
-
-          <div className="columns box has-margin-bottom-40">
-            <p className="column is-one-quarter has-text-weight-semibold has-text-primary has-margin-right-20">
-              {items.name}
-            </p>
-            <p className="column is-one-quarter has-margin-right-20">
-              <i className="fas fa-home"></i> {items.address}
-            </p>
-            <p className="column is-one-quarter has-margin-right-20">
-              <i className="fas fa-mobile-alt"></i> {items.phone}
-            </p>
-            <p className="column is-one-quarter has-margin-right-20">
-              <i className="fas fa-euro-sign"></i> {items.priceperday} EUR
-            </p>
-          </div>
-          <div className="columns">
-            <Link
-              to={`/auth/customers/edit/${props.match.params.id}`}
-              className="button is-info has-text-weight-semibold is-overlay has-margin-bottom-15 has-margin-right-30 is-fullwidth"
-            >
-              Edit customer
-            </Link>
-
-            <Link
-              onClick={() =>
-                axios
-                  .delete(`/api/customers/${props.match.params.id}`)
-                  .then(() => window.location.reload())
-                  .catch(err => console.log(err))
-              }
-              to="/auth/customers"
-              className="button is-danger has-text-weight-semibold has-margin-bottom-10 is-fullwidth"
-            >
-              Delete customer
-            </Link>
-          </div>
+          <CustomerBox
+            name={items.name}
+            address={items.address}
+            phone={items.phone}
+            id={props.match.params.id}
+          />
         </div>
       </div>
       <div className="columns">
@@ -71,41 +43,13 @@ function CustomerDetails(props) {
           </div>
           {items.pets &&
             items.pets.map(pet => (
-              <div key={pet._id} className="columns box has-margin-bottom-40">
-                <p className="column is-one-quarter has-text-weight-semibold has-text-primary has-margin-right-20">
-                  {pet.name}
-                </p>
-                <p className="column is-one-quarter has-margin-right-20">
-                  {pet.type}
-                </p>
-                <div className="column is-one-quarter has-margin-right-20">
-                  <p className="has-text-weight-semibold">Comments:</p>
-                  <p>{pet.comments}</p>
-                </div>
-                <div className="column is-one-quarter is-multiline">
-                  <Link
-                    to={`/auth/pet/edit/${pet._id}`}
-                    className="button is-info has-text-weight-semibold has-margin-bottom-15 has-margin-right-100 buttonwidth"
-                  >
-                    Edit
-                  </Link>
-
-                  <Link
-                    onClick={() =>
-                      axios
-                        .delete(`/api/pets/${pet._id}`)
-                        .then(() => window.location.reload())
-                        .catch(err => console.log(err))
-                    }
-                    to={`/auth/customers/${props.match.params.id}`}
-                    className="button is-danger has-text-weight-semibold has-margin-bottom-10 buttonwidth"
-                  >
-                    Delete
-                  </Link>
-                </div>
-
-                {/* <img src={pet.photo} alt="" /> */}
-              </div>
+              <PetBox
+                name={pet.name}
+                type={pet.type}
+                comments={pet.comments}
+                petid={pet.id}
+                customerid={props.match.params.id}
+              />
             ))}
 
           <div className="columns">
@@ -134,56 +78,17 @@ function CustomerDetails(props) {
 
           {items.jobs &&
             items.jobs.map(job => (
-              <div key={job._id} className="columns box has-margin-bottom-40">
-                <p className="column is-1 has-text-weight-semibold has-text-primary has-margin-right-20">
-                  {job.startdate}
-                </p>
-                <p className="column is-1 has-margin-right-20">{job.enddate}</p>
-                <div className="column is-1 has-margin-right-20">
-                  <p className="has-text-weight-semibold">Days:</p>
-                  <p>{job.numberofdays}</p>
-                </div>
-                <div className="column is-1 has-margin-right-20">
-                  <p className="has-text-weight-semibold">Daily Price:</p>
-                  <p>{items.priceperday}</p>
-                </div>
-                <div className="column is-1 has-margin-right-20">
-                  <p className="has-text-weight-semibold">Total:</p>
-                  <p>{job.totalprice}</p>
-                </div>
-                <div className="column is-1 has-margin-right-20">
-                  <p className="has-text-weight-semibold">Paid:</p>
-
-                  <p>{job.paid}</p>
-                </div>
-                <div className="column is-1 has-margin-right-20">
-                  <p className="has-text-weight-semibold">Comments:</p>
-                  <p>{job.description}</p>
-                </div>
-                {/* <div className="column is-one-quarter is-multiline">
-                  <Link
-                    to={`/auth/pet/edit/${pet._id}`}
-                    className="button is-info has-text-weight-semibold has-margin-bottom-15 has-margin-right-100 buttonwidth"
-                  >
-                    Edit
-                  </Link>
-
-                  <Link
-                    onClick={() =>
-                      axios
-                        .delete(`/api/pets/${pet._id}`)
-                        .then(() => window.location.reload())
-                        .catch(err => console.log(err))
-                    }
-                    to={`/auth/customers/${props.match.params.id}`}
-                    className="button is-danger has-text-weight-semibold has-margin-bottom-10 buttonwidth"
-                  >
-                    Delete
-                  </Link>
-                </div> */}
-
-                {/* <img src={pet.photo} alt="" /> */}
-              </div>
+              <JobBox
+                jobid={job.id}
+                startdate={job.startdate}
+                enddate={job.enddate}
+                numberofdays={job.numberofdays}
+                totalprice={job.numberofdays}
+                paid={job.paid}
+                archived={job.archived}
+                description={job.description}
+                customerid={props.match.params.id}
+              />
             ))}
 
           <button
