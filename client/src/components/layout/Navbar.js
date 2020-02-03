af9d951b50e815e78d38a4411d3d29f23b5df295
Monika <mswidzinska87@gmail.com>
2020-02-03 16:51:27 +0100

    ArchiveBox created

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { logout } from "../../actions/auth";
-import "./Burger.js";
 import "../../App.scss";
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
--- client/src/pages/CustomerDetails.js
+++ client/src/pages/CustomerDetails.js
@@ -5,6 +5,7 @@ import Calendar from "../components/myCalendar";
 import CustomerBox from "../components/CustomerBox";
 import PetBox from "../components/PetBox";
 import JobBox from "../components/JobBox";
+import ArchiveBox from "../components/ArchiveBox";
 // import { Link } from "react-router-dom";
 function CustomerDetails(props) {
   useEffect(() => {
@@ -104,9 +105,31 @@ function CustomerDetails(props) {
           </button>
         </div>
       </div>
-      <div className="columns"></div>
-      <div className="column is-8 is-offset-2 is-full-mobile is-paddingless has-padding-left-5-mobile has-padding-right-5-mobile mobileCalendarHeight has-margin-top-60">
-        <Calendar id={items._id} />
+      <div className="columns">
+        <div className="column is-8 is-offset-2 is-full-mobile is-paddingless has-padding-left-5-mobile has-padding-right-5-mobile mobileCalendarHeight has-margin-top-60">
+          <Calendar id={items._id} />
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column box is-8 is-offset-2 is-8-mobile is-offset-2-mobile has-margin-top-50 has-margin-bottom-50 has-padding-left-30 has-padding-right-30 has-padding-top-60">
+          {items.jobs &&
+            items.jobs.map(job => (
+              <ArchiveBox
+                id={items._id}
+                key={job._id}
+                jobid={job._id}
+                startdate={job.startdate}
+                enddate={job.enddate}
+                numberofdays={job.numberofdays}
+                priceperday={items.priceperday}
+                totalprice={job.totalprice}
+                paid={job.paid}
+                archived={job.archived}
+                description={job.description}
+                customerid={items._id}
+              />
+            ))}
+        </div>
       </div>
     </Fragment>
   ) : (
